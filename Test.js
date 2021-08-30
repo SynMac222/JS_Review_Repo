116318e874c84eb6ca781360e888281bdff37fc1
Yinan S <yinanshao765@gmail.com>
2021-08-30 03:34:53 -0500

    changing the meeting time

---
+++ Test.js
@@ -0,0 +1,10 @@
+cwebTestFunction();
+
+function cwebTestFunction(){
+
+    var name ="CF";
+    var subject ="web Tooling";
+    var meetingTime;
+    meetingTime ="4gitpm";
+    console.log('Name:$(name) Subject:$(subject) Meeting time:$(meetingTime)');
+}
