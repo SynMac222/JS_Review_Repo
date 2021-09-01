86db7857911f16576d38b980442b9b8afa65beaa
Yinan S <yinanshao765@gmail.com>
2021-09-01 03:51:56 -0500

    add variable and console output

--- Test.js
+++ Test.js
@@ -1,3 +1,5 @@
+
+
 cwebTestFunction();
 
 function cwebTestFunction(){
@@ -5,7 +7,10 @@ function cwebTestFunction(){
     var name ="C. Fulton";
     var subject ="web Tooling Classgit";
     var daysofweek="Monday,Wednesday,Friday"
-    var meetingTime;
+    var meetingTime,semesterLength;
     meetingTime ="4gitpm";
-    console.log('Name:$(name) Subject:$(subject) Meeting time:$(meetingTime)');
+    semesterLength=16;
+
+    console.log('Name: ${name} Subject:${subject} Meeting time:${meetingTime}');
+    console.log("the semester length is ${semesterLength}");
 }
